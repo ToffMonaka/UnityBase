d038b73e3d119b619aadcb0e3ae08f88d7da44b8
toff <toff-monaka@live.jp>
2023-04-07 17:40:04 +0900

    修正

---
+++ Assets/Scripts/ToffMonaka/Lib/Constant/Util.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * @file
+ * @brief Utilファイル
+ */
+
+namespace ToffMonaka.Lib.Constant
+{
+    /**
+     * @brief Utilクラス
+     */
+    public static class Util
+    {
+#if DEBUG
+    	public const bool DEBUG_FLAG = true;
+#else
+        public const bool DEBUG_FLAG = false;
+#endif
+        public const string NAME = "Toff Monaka Library";
+        public const string COMPANY_NAME = "Toff Monaka Project";
+        public const string VERSION_NAME = "1.0.0";
+    }
+}
---
+++ Assets/Scripts/ToffMonaka/UnityBase/Constant/Util.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * @file
+ * @brief Utilファイル
+ */
+
+namespace ToffMonaka.UnityBase.Constant
+{
+    /**
+     * @brief Utilクラス
+     */
+    public static class Util
+    {
+#if DEBUG
+        public const bool DEBUG_FLAG = true;
+#else
+        public const bool DEBUG_FLAG = false;
+#endif
+        public const string NAME = "C# ベースプロジェクト";
+        public const string COMPANY_NAME = "Toff Monaka Project";
+        public const string VERSION_NAME = "1.0.0";
+    }
+}
