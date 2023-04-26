9144188242aa33ebbe9bbda22886eca5d7d74589
toff <toff-monaka@live.jp>
2023-04-26 16:49:02 +0900

    修正

---
+++ Assets/Scripts/ToffMonaka/UnityBase/Scene/Test3DSubScene.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * @file
+ * @brief Test3DSubSceneファイル
+ */
+
+
+using UnityEngine;
+
+
+namespace ToffMonaka.UnityBase.Scene {
+/**
+ * @brief Test3DSubSceneクラス
+ */
+public class Test3DSubScene : ToffMonaka.Lib.Scene.SubScene
+{
+    /**
+     * @brief _OnStart関数
+     */
+    protected override void _OnStart()
+    {
+        return;
+    }
+
+    /**
+     * @brief _OnUpdate関数
+     */
+    protected override void _OnUpdate()
+    {
+        return;
+    }
+}
+}
